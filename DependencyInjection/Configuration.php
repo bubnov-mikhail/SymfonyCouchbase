1faec85e0bd12072258abf4c9f89c6772a581b14
Mihail Bubnov <mihail.bubnov@mediaopt.de>
2019-09-03 13:35:36 +0300

    Clean up Configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -33,9 +33,6 @@ class Configuration implements ConfigurationInterface
                     ->end()
                 ->end()
             ->end();
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
 
         return $treeBuilder;
     }
